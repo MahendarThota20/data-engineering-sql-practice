8cb1791f4a16f3f46e357701754aac78ab0c819c
MahendarThota <mahendarthota914@gmail.com>
2026-02-24 16:09:08 +0530

    sql_practice_1

---
+++ sql_question_1_table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE candidates (
+    candidate_id INT,
+    skill VARCHAR(50)
+);
+ 
+INSERT INTO candidates (candidate_id, skill) VALUES
+(123, 'Python'),
+(123, 'Tableau'),
+(123, 'PostgreSQL'),
+(234, 'R'),
+(234, 'PowerBI'),
+(234, 'SQL Server'),
+(345, 'Python'),
+(345, 'Tableau');  
